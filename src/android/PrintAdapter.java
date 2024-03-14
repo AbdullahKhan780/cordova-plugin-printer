f19e12c2bc6cf706d46dc82381b68b9dcaa344b9
Saif Ali Khan <mohammadabdullahkhan@mail.com>
2024-03-14 17:44:03 +0500

    Upgraded the android code

--- src/android/PrintAdapter.java
+++ src/android/PrintAdapter.java
@@ -1,5 +1,5 @@
 /*
- Copyright 2013 Sebastián Katzer
+ Copyright 2024 Abdullah Khan
 
  Licensed to the Apache Software Foundation (ASF) under one
  or more contributor license agreements.  See the NOTICE file
@@ -28,8 +28,10 @@ import android.print.PageRange;
 import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintDocumentInfo;
-import android.support.annotation.NonNull;
-import android.support.v4.print.PrintHelper;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.print.PrintHelper;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -39,93 +41,95 @@ import java.io.OutputStream;
 import static android.print.PrintDocumentInfo.CONTENT_TYPE_DOCUMENT;
 
 /**
- * Document adapter to render and print PDF files.
- */
-class PrintAdapter extends PrintDocumentAdapter
-{
-    // The name of the print job
-    private final @NonNull String jobName;
-
-    // Max page count
-    private final int pageCount;
-
-    // The input stream to render
-    private final @NonNull InputStream input;
-
-    // The callback to inform once the job is done
-    private final @NonNull PrintHelper.OnPrintFinishCallback callback;
-
-    /**
-     * Constructor
-     *
-     * @param jobName   The name of the print job.
-     * @param pageCount The max page count.
-     * @param input     The input stream to render.
-     * @param callback  The callback to inform once the job is done.
-     */
-    PrintAdapter (@NonNull String jobName, int pageCount,
+  * Document adapter to render and print PDF files.
+  */
+ class PrintAdapter extends PrintDocumentAdapter {
+     // The name of the print job
+     private final @NonNull String jobName;
+ 
+     // Max page count
+     private final int pageCount;
+ 
+     // The input stream to render
+     private final @NonNull InputStream input;
+ 
+     // The callback to inform once the job is done
+     private final @NonNull PrintHelper.OnPrintFinishCallback callback;
+ 
+     /**
+      * Constructor
+      *
+      * @param jobName   The name of the print job.
+      * @param pageCount The max page count.
+      * @param input     The input stream to render.
+      * @param callback  The callback to inform once the job is done.
+      */
+     PrintAdapter(@NonNull String jobName, int pageCount,
                   @NonNull InputStream input,
-                  @NonNull PrintHelper.OnPrintFinishCallback callback)
-    {
-        this.jobName   = jobName;
-        this.pageCount = pageCount;
-        this.input     = input;
-        this.callback  = callback;
-    }
-
-    @Override
-    public void onLayout (PrintAttributes oldAttributes,
+                  @NonNull PrintHelper.OnPrintFinishCallback callback) {
+         this.jobName = jobName;
+         this.pageCount = pageCount;
+         this.input = input;
+         this.callback = callback;
+     }
+ 
+     @Override
+     public void onLayout(PrintAttributes oldAttributes,
                           PrintAttributes newAttributes,
                           CancellationSignal cancellationSignal,
                           LayoutResultCallback callback,
-                          Bundle bundle)
-    {
-        PrintDocumentInfo pdi;
-
-        if (cancellationSignal.isCanceled())
-            return;
-
-        pdi = new PrintDocumentInfo.Builder(jobName)
-                .setContentType(CONTENT_TYPE_DOCUMENT)
-                .setPageCount(pageCount)
-                .build();
-
-        boolean changed = !newAttributes.equals(oldAttributes);
-
-        callback.onLayoutFinished(pdi, changed);
-    }
-
-    @Override
-    public void onWrite (PageRange[] range,
+                          Bundle bundle) {
+         PrintDocumentInfo pdi;
+ 
+         if (cancellationSignal.isCanceled())
+             return;
+ 
+         pdi = new PrintDocumentInfo.Builder(jobName)
+                 .setContentType(CONTENT_TYPE_DOCUMENT)
+                 .setPageCount(pageCount)
+                 .build();
+ 
+         boolean changed = !newAttributes.equals(oldAttributes);
+ 
+         callback.onLayoutFinished(pdi, changed);
+     }
+ 
+     @Override
+     public void onWrite(PageRange[] range,
                          ParcelFileDescriptor dest,
                          CancellationSignal cancellationSignal,
-                         WriteResultCallback callback)
-    {
-        if (cancellationSignal.isCanceled())
-            return;
-
-        OutputStream output = new FileOutputStream(dest.getFileDescriptor());
-
-        try {
-            PrintIO.copy(input, output);
-        } catch (IOException e) {
-            callback.onWriteFailed(e.getMessage());
-            return;
-        }
-
-        callback.onWriteFinished(new PageRange[]{ PageRange.ALL_PAGES });
-    }
-
-    /**
-     * Closes the input stream and invokes the callback.
-     */
-    @Override
-    public void onFinish ()
-    {
-        super.onFinish();
-
-        PrintIO.close(input);
-
-        callback.onFinish();
-    }
-}
+                         WriteResultCallback callback) {
+         if (cancellationSignal.isCanceled())
+             return;
+ 
+         OutputStream output = new FileOutputStream(dest.getFileDescriptor());
+ 
+         try {
+             byte[] buf = new byte[1024];
+             int bytesRead;
+             while ((bytesRead = input.read(buf)) > 0) {
+                 output.write(buf, 0, bytesRead);
+             }
+             callback.onWriteFinished(new PageRange[]{PageRange.ALL_PAGES});
+         } catch (IOException e) {
+             callback.onWriteFailed(e.getMessage());
+         } finally {
+             try {
+                 input.close();
+                 output.close();
+             } catch (IOException e) {
+                 Log.e("PrintAdapter", "Failed to close stream", e);
+             }
+         }
+     }
+ 
+     /**
+      * Closes the input stream and invokes the callback.
+      */
+     @Override
+     public void onFinish() {
+         super.onFinish();
+ 
+         callback.onFinish();
+     }
+ }
--- src/android/Printer.java
+++ src/android/Printer.java
@@ -1,29 +1,8 @@
-/*
- Copyright 2013 Sebastián Katzer
-
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
- */
-
 package de.appplant.cordova.plugin.printer;
-
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -37,8 +16,7 @@ import org.json.JSONObject;
  * that loads the markup data. Once the page has been fully rendered it takes
  * the print adapter of that web view and initializes a print job.
  */
-public final class Printer extends CordovaPlugin
-{
+public final class Printer extends CordovaPlugin {
     /**
      * Executes the request.
      *
@@ -56,24 +34,16 @@ public final class Printer extends CordovaPlugin
      * @return         Whether the action was valid.
      */
     @Override
-    public boolean execute (String action, JSONArray args,
-                            CallbackContext callback)
-    {
+    public boolean execute(String action, JSONArray args, CallbackContext callback) {
         boolean valid = true;
 
-        if (action.equalsIgnoreCase("check"))
-        {
+        if (action.equalsIgnoreCase("check")) {
             check(args.optString(0), callback);
-        }
-        else if (action.equalsIgnoreCase("types"))
-        {
+        } else if (action.equalsIgnoreCase("types")) {
             types(callback);
-        }
-        else if (action.equalsIgnoreCase("print"))
-        {
+        } else if (action.equalsIgnoreCase("print")) {
             print(args.optString(0), args.optJSONObject(1), callback);
-        }
-        else {
+        } else {
             valid = false;
         }
 
@@ -86,10 +56,9 @@ public final class Printer extends CordovaPlugin
      * @param item     Any kind of URL like file://, file:///, res:// or base64://
      * @param callback The plugin function to invoke with the result.
      */
-    private void check (@Nullable String item, CallbackContext callback)
-    {
+    private void check(@Nullable String item, CallbackContext callback) {
         cordova.getThreadPool().execute(() -> {
-            PrintManager pm   = new PrintManager(cordova.getContext());
+            PrintManager pm = new PrintManager(cordova.getContext());
             boolean printable = pm.canPrintItem(item);
 
             sendPluginResult(callback, printable);
@@ -101,13 +70,11 @@ public final class Printer extends CordovaPlugin
      *
      * @param callback The plugin function to invoke with the result.
      */
-    private void types (CallbackContext callback)
-    {
+    private void types(CallbackContext callback) {
         cordova.getThreadPool().execute(() -> {
             JSONArray utis = PrintManager.getPrintableTypes();
 
-            PluginResult res = new PluginResult(
-                    Status.OK, utis);
+            PluginResult res = new PluginResult(Status.OK, utis);
 
             callback.sendPluginResult(res);
         });
@@ -121,14 +88,12 @@ public final class Printer extends CordovaPlugin
      * @param settings Additional settings how to render the content.
      * @param callback The plugin function to invoke with the result.
      */
-    private void print (@Nullable String content, JSONObject settings,
-                        CallbackContext callback)
-    {
+    private void print(@Nullable String content, JSONObject settings, CallbackContext callback) {
         cordova.getThreadPool().execute(() -> {
             PrintManager pm = new PrintManager(cordova.getContext());
-            WebView view    = (WebView) webView.getView();
+            WebView view = (WebView) webView.getView();
 
-            pm.print(content, settings, view, (boolean completed) -> sendPluginResult(callback, completed));
+            pm.print(content, settings, view, completed -> sendPluginResult(callback, completed));
         });
     }
 
@@ -138,9 +103,7 @@ public final class Printer extends CordovaPlugin
      * @param callback The callback to invoke.
      * @param value    The argument to pass with.
      */
-    private void sendPluginResult (@NonNull CallbackContext callback,
-                                   boolean value)
-    {
+    private void sendPluginResult(@NonNull CallbackContext callback, boolean value) {
         PluginResult result = new PluginResult(Status.OK, value);
 
         callback.sendPluginResult(result);
